c5c1a21a0e287db6cdc717c97b7355b016840e79
ABHISHEK KR. GUPTA <abhishekkgupta20_ug@ece.nits.ac.in>
2024-08-26 15:00:19 +0530

    Added:Folder Structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './components/home/header/header.component';
+import { BooksContainerComponent } from './components/home/books-container/books-container.component';
+import { ForgetPasswordComponent } from './components/authentication/forget-password/forget-password.component';
+import { LoginSignupComponent } from './components/authentication/login-signup/login-signup.component';
+import { UserProfileComponent } from './components/home/user-profile/user-profile.component';
+import { BookDetailComponent } from './components/home/book-detail/book-detail.component';
+import { MyOrderComponent } from './components/home/my-order/my-order.component';
+import { WishlistComponent } from './components/home/wishlist/wishlist.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BooksContainerComponent,
+    ForgetPasswordComponent,
+    LoginSignupComponent,
+    UserProfileComponent,
+    BookDetailComponent,
+    MyOrderComponent,
+    WishlistComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/authentication/login-signup/login-signup.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-signup',
+  templateUrl: './login-signup.component.html',
+  styleUrls: ['./login-signup.component.scss']
+})
+export class LoginSignupComponent {
+
+}

a1f99f027f5a7c8782cc84f3d0aa85dd062317c3
Hanuman Boddu <Hanuman.Boddu@godigit.com>
2024-08-26 17:05:24 +0530

    Header Component UI Done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './components/home/header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'header',component:HeaderComponent}, //Navbar Component
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
